404abd4f9b94f3f33f69ef1fff9bf38d4895c1d8
Yashsaxenago <yashsaxenaald@gmail.com>
2022-07-19 16:25:48 +0530

    first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import JSONDATA from './MOCK_DATA.json';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Search Your Game and Enjoy</h1>
+      <input type="text" placeholder="search your game"
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
+      {JSONDATA.filter((val) => {
+        if (searchTerm === "") {
+          return val;
+        } else if  (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+          
+          return val;
+        }
+      }).map((val, key) => {
+        return (
+          <div className="user" key={key}>
+            <p>Game Name: {val.title}</p>
+            <p>Game Platform: {val.platform}</p>
+            <p>Game Score:{val.score}</p>
+            <p>Game Genre; {val.genre}</p>
+            <p>Game Editor_Choice:{val.editors_choice}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
